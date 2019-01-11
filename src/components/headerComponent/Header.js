49ead6000f66e80109631b1beb369b5f5f81427c
antoinettelovell <43275088+antoinettelovell@users.noreply.github.com>
2019-01-11 16:04:38 -0500

    veggie-boy-pizza changes

--- src/components/headerComponent/Header.js
+++ src/components/headerComponent/Header.js
@@ -7,17 +7,19 @@ class Header extends Component {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark" id="navbar-bg">
         <a className="navbar-brand" href="#"></a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse"
+         data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" 
+         aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <Link to="/" class="nav-item nav-link">Home</Link>
-            <Link to="/menu" class="nav-item nav-link">Menu</Link>
-            <Link to="/order" class="nav-item nav-link">Order</Link>
-            <Link to="/about" class="nav-item nav-link">About</Link>
-            <Link to="/login" class="nav-item nav-link">Existing Customer</Link>
-            <Link to="/register" class="nav-item nav-link">New Customer</Link>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link to="/" className="nav-item nav-link">Home</Link>
+            <Link to="/menu" className="nav-item nav-link">Menu</Link>
+            <Link to="/order" className="nav-item nav-link">Order</Link>
+            <Link to="/about" className="nav-item nav-link">About</Link>
+            <Link to="/login" className="nav-item nav-link">Existing Customer</Link>
+            <Link to="/register" className="nav-item nav-link">New Customer</Link>
                      <div className="white-font" id="brick">
                   
            <h4>Brick Oven Pizza</h4>
--- src/components/pages/Menu.js
+++ src/components/pages/Menu.js
@@ -9,7 +9,7 @@ import Login from '../Home/Login';
  class Menu extends Component {
   render() {
     return (
-  <div className="container-fluid">
+  <div className="container-fluid font-white">
          <div id="tickerwrap">
     <marquee id="ticker">
         Welcome to Veggie Boy Pizza. We make the best pizza 
@@ -65,6 +65,104 @@ import Login from '../Home/Login';
 </div>
 <button className="onClick"> Order Now</button>
 <br/>
+<h4>Barbecue Pizza</h4>
+< img src= {require( "../../images/barbecued.jpeg")} alt= "barbecue pizza" width="200" height="150"/>
+<hr/>
+<div>
+    <p>
+        The one and only barbecue pizza is very delicious. Have one  for $8.99
+    </p>
+    <hr/>
+</div>
+<button className="onClick"> Order Now</button>
+<br/>
+<h4>Brickoven Pizza</h4>
+< img src= {require( "../../images/brickoven.jpg")} alt= "brickoven pizza" width="200" height="150"/>
+<hr/>
+<div>
+    <p>
+        The one and only brickoven pizza is very delicious. Have one  for $8.99
+    </p>
+    <hr/>
+</div>
+<button className="onClick"> Order Now</button>
+<br/>
+<h4>Heart Shaped Pizza</h4>
+< img src= {require( "../../images/heart-shaped-pizza.jpg")} alt= "heartshaped pizza" width="200" height="150"/>
+<hr/>
+<div>
+    <p>
+        The one and only heart shaped pizza is very delicious. Have one  for $8.99
+    </p>
+    <hr/>
+</div>
+<button className="onClick"> Order Now</button>
+<br/>
+<h4>Flat Bread Delight Pizza</h4>
+< img src= {require( "../../images/flatbreaddelight.jpg")} alt= "flat bread pizza" width="200" height="150"/>
+<hr/>
+<div>
+    <p>
+        The one and only flat bread pizza is very delicious. Have one  for $8.99
+    </p>
+    <hr/>
+</div>
+<button className="onClick"> Order Now</button>
+<br/>
+<h4>Pesto Pizza</h4>
+< img src= {require( "../../images/pesto.jpg")} alt= "pesto pizza" width="200" height="150"/>
+<hr/>
+<div>
+    <p>
+        The one and only pesto pizza is very delicious. Have one  for $8.99
+    </p>
+    <hr/>
+</div>
+<button className="onClick"> Order Now</button>
+<br/>
+<h4>Pizza Board Pizza</h4>
+< img src= {require( "../../images/pizzaboard.jpg")} alt= "pizza board pizza" width="200" height="150"/>
+<hr/>
+<div>
+    <p>
+        The one and only pizza board pizza is very delicious. Have one  for $8.99
+    </p>
+    <hr/>
+</div>
+<button className="onClick"> Order Now</button>
+<br/>
+<h4>Simply Veggie Pizza</h4>
+< img src= {require( "../../images/simplyveggie.jpeg")} alt= "simply veggie pizza" width="200" height="150"/>
+<hr/>
+<div>
+    <p>
+        The one and only simply veggie pizza is very delicious. Have one  for $8.99
+    </p>
+    <hr/>
+</div>
+<button className="onClick"> Order Now</button>
+<br/>
+<h4>Spinach Pizza</h4>
+< img src= {require( "../../images/spinach.jpg")} alt= "spinach pizza" width="200" height="150"/>
+<hr/>
+<div>
+    <p>
+        The one and only spinach pizza is very delicious. Have one  for $8.99
+    </p>
+    <hr/>
+</div>
+<button className="onClick"> Order Now</button>
+<br/>
+<h4>Zucchini Goat Cheese Pizza</h4>
+< img src= {require( "../../images/zucchinigoatcheese.jpg")} alt= "zucchini goat cheese pizza" width="200" height="150"/>
+<hr/>
+<div>
+    <p>
+        The one and only zucchini Goat Cheese pizza is very delicious. Have one  for $8.99
+    </p>
+    <hr/>
+</div>
+<button className="onClick"> Order Now</button>
       </div>
   </div>
     )
