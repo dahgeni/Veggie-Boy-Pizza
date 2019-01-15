d78f26c48972eee7eeaf410da819e2bda89f97b6
tyron hawkins <tyron.hawkins@gmail.com>
2019-01-15 14:48:25 -0500

    added alert feature and fixed border in form

--- src/components/Home/Reservation.js
+++ src/components/Home/Reservation.js
@@ -3,40 +3,59 @@ import React, { Component } from 'react'
     class Reservation extends Component {
   render() {
     return (
+      
       <div className="background " id="reservationbg">
-      <div className= "container"> 
-      <div className="font-white">
-      <h1> Reserve A Table </h1> 
-      <br/>     
-     <div className="form-group">
-         <label>First and Last Name</label>
-         <input required class="form-control"        name="name"
-         placeholder="Enter First and Last Name..."  /> 
-     </div>
-                   
-     <div className="form-group">
-         <label>Email</label>
-         <input class="form-control" name="email" placeholder="Enter Email..." />
-     </div>
-     <div className="form-group">
-         <label> Number of People</label>
-         <input className="form-control" name="How Many People"  placeholder="How Many Guest ..." />
-     </div>
-     <div className="form-group">
-         <label>Desired Date</label>
-         <input className="form-control" name="date"  placeholder="Enter Date ..." />
-     </div>
-
-     <div className="form-group">
-         <label>Desired Time</label>
-         <input class="form-control" name="lastName"  placeholder="Enter Time..." />
-     </div>
-                   
-     <button className="btn btn-danger btn-dark">Submit</button>
-     <br />
-     </div>
-     </div>
-     </div>
+        <div className= "container"> 
+          <div className="font-white">
+            <h1> Reserve A Table </h1> 
+            <br/>     
+            <div className="form-group2">
+              <label>First and Last Name</label>
+              <input required class="form-control" name="name" placeholder="Enter First and Last Name..."  /> 
+            </div>
+            <div className="form-group2">
+              <label>Email</label>
+              <input class="form-control" name="email" placeholder="Enter Email..." />
+            </div>
+            <div className="form-group2">             
+              <label> Number of People</label>
+              <input className="form-control" name="How Many People"  placeholder="How Many Guest ..." />
+            </div>
+            <div className="form-group2">
+              <label>Desired Date</label>
+              <input className="form-control" name="date"  placeholder="Enter Date ..." />
+            </div>
+            <div className="form-group2">
+              <label>Desired Time</label>
+              <input className="form-control" name="time" placeholder="Enter Time..." />
+            </div>
+             <br />
+             <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#pizzaModal">
+              Submit
+            </button>
+            <br />
+         </div>
+        </div>
+        <div class="modal fade" id="pizzaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h4 class="modal-title customColor " id="exampleModalCenterTitle ">Veggie Boy Pizza "Reservation" Confirmation</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <h3 className="modal-body" >
+                Your Table Has Been Reserve. Thanks For Your Business!
+              </h3>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
