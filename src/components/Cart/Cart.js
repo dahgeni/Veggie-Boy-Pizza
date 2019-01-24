f230451df3703279fa05c0b866144749a191dfca
tyron hawkins <tyron.hawkins@gmail.com>
2019-01-24 09:37:51 -0500

    cart and its css

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -19,40 +19,40 @@ import './cart.css';
         <div className="row-4 p-row">
          
         <h3>Items(s)</h3>
-        </div>
+      
         </div>  
       <div className="row my-row"> 
-      </div>
       <div className="col-6 my-col">
               <h4>Price:</h4>
            
-                          
-          <div className="col-4 my-col">
-          </div>
-         </div>
-         
+        </div>
+        </div>
        <div className="row my-row">     
-      <div className="col-6 my-col">             
-          <h4>Subtotal:</h4> 
+      <div className="col-6 my-col">                  <h4>Subtotal:</h4> 
           </div>
       </div>
-         <div className="col-4 my-col">
+       
+   
+     </div>
       </div>
       </div>
-     
-      
-
       <br />
      
      
       
       <button type="button"className="btn btn-warning big-font"> Delete</button>
       <button type="button"className="btn btn-dark big-font float-left"> Buy Now</button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
    
-      <button type="button" className="btn btn-light  align-right" >Logout</button>
+      <button type="button" className="btn btn-light" ><h4>Logout</h4></button>
 
       </div>
-      </div>
+    
      
     
 
