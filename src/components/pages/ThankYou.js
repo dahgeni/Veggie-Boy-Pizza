cb1b0f9cd2e18f21e82b338894788d8e303320be
antoinettelovell <43275088+antoinettelovell@users.noreply.github.com>
2019-01-24 10:45:12 -0500

    fixing thankyou page

--- src/components/pages/ThankYou.js
+++ src/components/pages/ThankYou.js
@@ -8,25 +8,15 @@ import Login from '../Home/Login';
 
 const ThankYou = () => {
   return (
-    <img src="https://thumbs.dreamstime.com/z/thank-you-card-bright-colors-stylish-floral-background-text-berries-leaves-flower-46494488.jpg"
-      height="500" width="800"></img>
-<div>
-
-    < a className="btn btn-primary btn-block">Websites</a>
-    <button class="btn btn-danger btn-block">Logout</button>
-    <br />
-    <a className="btn btn-outline-dark" routerLink="/user-list">User List</a>
-  
-</div >
-<div>
-  <nav className="navbar navbar-dark bg-primary fixed-bottom">
-    <div className="full-width">
-      <a className="color-white float-right">
-        <i className="fas fa-user" />
-      </a>
+    <div>
+      <img src="https://rlv.zcache.co.uk/a_stylish_thank_you_heart_stickers-rb9688721e9304d09b74ad9d14b2eeda8_v9w0n_8byvr_307.jpg?rvtype=content"
+        height="400" width="1100"></img>
+      <br/>
+      <br/>
+      < a className="btn btn-secondary btn-block">Thank You</a>
+      <button className="btn btn-success btn-block">Logout</button>
+      <br />
     </div>
-  </nav>
-</div>
   )
 }
 
