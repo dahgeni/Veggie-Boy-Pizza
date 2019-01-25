bca449fdcbf02c75326a53ad5fc6f45b9bec97a3
tyron hawkins <tyron.hawkins@gmail.com>
2019-01-25 14:43:39 -0500

    build with remove from gitignore and change dist to build

---
+++ build/precache-manifest.6eede919a3352eebe1c5e65c8f5dd875.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "be312d27376c60f151fc02e7342a3222",
+    "url": "/static/media/mushroom.be312d27.jpg"
+  },
+  {
+    "revision": "87bf5ac633bad75b108e",
+    "url": "/static/css/main.d9e2c040.chunk.css"
+  },
+  {
+    "revision": "c231cb324f4a5c4ae82b",
+    "url": "/static/js/1.c231cb32.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f190a27bcad92ba5b553da5c7ac87086",
+    "url": "/static/media/restaurantseating.f190a27b.jpg"
+  },
+  {
+    "revision": "a67b3c9754b3dba462cbf9b5cf5edc19",
+    "url": "/static/media/brickoven.a67b3c97.jpg"
+  },
+  {
+    "revision": "4af8fa31253f8c692210f4855d4fe01e",
+    "url": "/static/media/pepper.4af8fa31.jpg"
+  },
+  {
+    "revision": "4e2abf20a34a4737312f3543d8f5bc48",
+    "url": "/static/media/cheesepizza.4e2abf20.jpg"
+  },
+  {
+    "revision": "a59694d388e3f63a9723e4e15b149227",
+    "url": "/static/media/onion.a59694d3.jpg"
+  },
+  {
+    "revision": "87bf5ac633bad75b108e",
+    "url": "/static/js/main.87bf5ac6.chunk.js"
+  },
+  {
+    "revision": "75c2cac981ecd69e8ae8e4e3965ff6d5",
+    "url": "/static/media/barbecued.75c2cac9.jpeg"
+  },
+  {
+    "revision": "c7e23f16067d562fc726c3cc2e552943",
+    "url": "/static/media/olive pizza.c7e23f16.jpg"
+  },
+  {
+    "revision": "250a02eea591f19cae223856a17f5d93",
+    "url": "/static/media/heart-shaped-pizza.250a02ee.jpg"
+  },
+  {
+    "revision": "9bb2e4c50100849c945490386f4dc022",
+    "url": "/static/media/flatbreaddelight.9bb2e4c5.jpg"
+  },
+  {
+    "revision": "1d716a36fef35f453b5ba2d82c3d0aff",
+    "url": "/static/media/pesto.1d716a36.jpg"
+  },
+  {
+    "revision": "c564b91a47c603e83778337884b513a7",
+    "url": "/static/media/simplyveggie.c564b91a.jpeg"
+  },
+  {
+    "revision": "91ef2665758dd5f18d0446d45041aa17",
+    "url": "/static/media/spinach.91ef2665.jpg"
+  },
+  {
+    "revision": "e88ab9280cf1e76908d5a420f13b755a",
+    "url": "/static/media/zucchinigoatcheese.e88ab928.jpg"
+  },
+  {
+    "revision": "4548ae5010ae03a358a573ec1977ddd5",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, "build")));
 
 // CORS - Cross Origin Resource Sharing
 app.use(function(req, res, next) {
@@ -55,7 +55,7 @@ const server = http.createServer(app);
 require("./server/app")(app);
 
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "dist/index.html"));
+  res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
 // server.listen(port);
